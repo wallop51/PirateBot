5e2b789e7a7637b5cc14cf4aad36ca0eb8cdb921
jdf18 <97624626+jdf18@users.noreply.github.com>
2022-02-04 17:17:27 +0000

    Updated main.py
    Added comments/altered code to help with readability

--- main.py
+++ main.py
@@ -1,28 +1,54 @@
 import discord
 from discord.ext import tasks
-import os
+import os, time
 from pirategame import Server
+from constants import Constants
+from json import dumps, loads
 
+# Start the discord client
 client = discord.Client()
 
+#* When client starts running:
 @client.event
 async def on_ready():
+    # print response
     print('Logged in as {0.user}'.format(client))
+
+    # start discord.ext.tasks loop
     update_server.start()
 
 @tasks.loop(seconds=5.0)
 async def update_server():
+    #* Get the current game server
     global game_server
+
+    #* Send data to the server for it to parse in .update()
     
+    # Get the members currently in the voice chat and send to the server
     game_server.members = client.get_channel(game_server.VOICE_CHANNEL_ID).members
-    # TODO check for people leaving/joining the vc
+    #TODO check for people leaving/joining the vc
     
+    #* call the update method on the server and parse all responses
     for response in game_server.update():
+        # Format:
+        # {
+        #   'type':['message'],
+        #   'subject:['all', member_id], # (who to send the message to)
+        #   'content':'string containing the contents of the message'
+        # }
+
+        # ? Option to send messages to the server chat? Any reason this should be a feature?
+
         if response['type'] == 'message':
             if response['subject'] == 'all':
                 for member in game_server.members:
+                    # Print a message log
                     print(f'sending {member.id} {response["content"]}')
                     await member.send(response['content'])
+            else:
+                # send a message to a specific person
+                pass
+
             
 
 @client.event
@@ -31,29 +57,44 @@ async def on_message(message):
     # message.author is who sent the message
     
     if message.author == client.user:
-        return
+        return # Exit if the bot (client) sent the message
+
+    #* If the channel is a DM to a user:
     if type(message.channel) == discord.channel.DMChannel:
         response = game_server.message_recieved(message.author, message.content)
         if response:
             await message.author.send(response)
-            
+    
+    #* If the channel is on a server:
     elif type(message.channel) == discord.channel.TextChannel:
         # * Command Parsing
         if message.content.startswith('!pirate') and message.channel.id == game_server.COMMAND_CHANNEL_ID:
             parts = message.content.split(' ')
             if parts[1].upper() == 'START':
                 game_server.start_game()
+        else:
+            # Ignore this as not a command
+            pass
 
 def main():
     global game_server
+
+    # Init the game server
     game_server = Server(voice=938879048455712898, command=938902248946274364)
+    #TODO need to make the bot get channel id for current vc
+    #TODO need to be able to host multiple games at once
+
+    # Start the discord client
+    # TOKEN value is read from .env file
     client.run(TOKEN)
     
 if __name__ == '__main__':
+    TOKEN = None
     with open('.env') as file:
         for line in file.readlines():
             parts = line.split('=')
             exec('{}="{}"'.format(parts[0], parts[1]))
+            # Reads value for TOKEN and saves to TOKEN
 
     main()
 
