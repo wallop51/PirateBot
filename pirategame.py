33a8d8abbc95c30170d43ce0301887209c80b616
jdf18 <97624626+jdf18@users.noreply.github.com>
2022-02-04 17:20:36 +0000

    Updated pirategame.py
    Improved readability by adding comments/altering code and added private messaging support for the server (need to implement this in the discord client)

--- pirategame.py
+++ pirategame.py
@@ -1,35 +1,60 @@
 class Server:
-    def __init__(self, voice, command):
-        self.VOICE_CHANNEL_ID = voice
-        self.COMMAND_CHANNEL_ID = command
-        self.started = False
-        self.broadcasts = []
+    def __init__(self, voice: int, command: int):
+        # Create constants for channel IDs
+        self.VOICE_CHANNEL_ID: int = voice
+        self.COMMAND_CHANNEL_ID: int = command
+
+        # Create game control properties
+        self.started: bool = False
+
+        # Create messaging lists
+        self.broadcasts: list = []
+        self.messages: list = []
         
-    def message_recieved(self, author, content):
+    def message_recieved(self, author: str, content: str) -> str:
         outbound_message = None
 
-        outbound_message = f'{author} sent a message saying {content}'
+        outbound_message: str = f'{author} sent a message saying {content}'
 
         return outbound_message
 
     def start_game(self):
         if self.started:
             return
-        self.started = True
+        self.started: bool = True
         self.broadcasts.append('GAME STARTING')
+
+    
+    def direct_message(self, subject, content):
+        self.messages.append((subject, content))
             
     
     def update(self):
         # send messages to everyone
         # other stuff
         
-        outbound = []
+        outbound: list = []
+
+        copy = self.messages.copy()
+        for message in copy:
+            outbound.append(
+                {
+                    'type':'message',
+                    'subject':message[0],
+                    'content':message[1],
+                }
+            )
+            #TODO discord client side
 
         copy = self.broadcasts.copy()
         for broadcast_message in copy:
-            outbound.append({'type':'message',
-             'subject':'all',
-             'content':broadcast_message})
+            outbound.append(
+                {
+                    'type':'message',
+                    'subject':'all',
+                    'content':broadcast_message,
+                }
+            )
             self.broadcasts.remove(broadcast_message)  
         if outbound:
             return outbound
